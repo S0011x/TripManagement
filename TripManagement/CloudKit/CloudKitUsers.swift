c75b9328daf4fcfd8c5f61f4a18e40898ced2586
S0011x <suha.alrajhi01@gmail.com>
2024-04-22 12:11:48 +0300

    cloud connecting

---
+++ TripManagement/CloudKit/CloudKitUsers.swift
@@ -0,0 +1,18 @@
+//
+//  CloudKitUsers.swift
+//  TripManagement
+//
+//  Created by suha alrajhi on 12/10/1445 AH.
+//
+
+import SwiftUI
+
+struct CloudKitUsers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CloudKitUsers()
+}
